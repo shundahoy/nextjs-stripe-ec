7f742e5672ee78e634bf6e3ad66c02427dfd6dd3
shundahoy <shundahoy@gmail.com>
2022-05-25 07:35:44 +0900

    コミットテスト

---
+++ pages/api/products.js
@@ -0,0 +1,39 @@
+import Stripe from "stripe";
+
+export default async function handler(req, res) {
+  // GET以外のリクエストを許可しない
+  if (req.method.toLocaleLowerCase() !== "get") {
+    return res.status(405).end();
+  }
+  const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+    apiVersion: "2020-08-27", // StripeのAPIバージョンを指定
+    maxNetworkRetries: 3, // ネットワークエラーでStripe API呼び出しが失敗した時のリトライ回数を指定
+  });
+  const products = await stripe.products.list();
+  if (!products.data || products.data.length < 1) {
+    return res.status(200).json([]);
+  }
+  const response = await Promise.all(
+    products.data.map(async (product, i) => {
+      const prices = await stripe.prices.list({
+        product: product.id,
+      });
+      return {
+        id: product.id,
+        description: product.description,
+        name: product.name,
+        images: product.images,
+        unit_label: product.unit_label,
+        prices: prices.data.map((price) => {
+          return {
+            id: price.id,
+            currency: price.currency,
+            transform_quantity: price.transform_quantity,
+            unit_amount: price.unit_amount,
+          };
+        }),
+      };
+    })
+  );
+  res.status(200).json(response);
+}
---
+++ pages/hello-world.jsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+export default function HelloWorld(props) {
+    return (
+        <div>
+            <Head>
+                <title>Hello World</title>
+                <meta name="description" content="検索エンジン用の説明文" />
+            </Head>
+            <h1>Hello World</h1>
+             <pre><code>{JSON.stringify(props,null,2)}</code></pre>
+        </div>
+    )
+ }
+
+export async function getServerSideProps (context) {
+    try {
+        const host = context.req.headers.host || 'localhost:3000'
+        const protocol = /^localhost/.test(host) ? 'http' : 'https' 
+        const products = await fetch(`${protocol}://${host}/api/products`)
+            .then(data => data.json())
+        return {
+            props: {
+                products,
+            }
+        }
+    } catch (e) {
+        console.log(e)
+        return {
+            props: {
+                products: [],
+            }
+        }
+    }
+}
